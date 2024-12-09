58652aed6b9a18365b428e508f5e1ba503248159
rp-honorato <rpedro.honorato@gmail.com>
2024-12-09 17:01:05 +0000

    MAP / iterator

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,30 +1,46 @@
-import './style.css'
-import Trash from '../../assets/Trash.svg'
+import "./style.css";
+import Trash from "../../assets/Trash.png";
+
 function Home() {
+  const users = [
+    {
+      id: "diihiheh298333h3yb22fubufb23",
+      name: "Pedro Honorato",
+      age: 26,
+      email: "pedro@pedro.com",
+    },
+    {
+      id: "diihiheh298333h3yb22fubdddufb23",
+      name: "Pedro Silva",
+      age: 27,
+      email: "pedro@pedro.com",
+    },
+  ];
 
   return (
-      <div className='container'>
+    <div className="container">
       <form>
         <h1>Cadastro de Usuários</h1>
-        <input name="nome" type="text" placeholder='Nome'/>
-        <input name="idade" type="number" placeholder='Idade'/>
-        <input name="email" type="email" placeholder='E-mail'/>
-        <button type=''>Cadastrar</button>
+        <input name="nome" type="text" placeholder="Nome" />
+        <input name="idade" type="number" placeholder="Idade" />
+        <input name="email" type="email" placeholder="E-mail" />
+        <button type="">Cadastrar</button>
       </form>
-
-      <div>
-        <div>
-          <p>Nome: </p>
-          <p>Idade: </p>
-          <p>Email: </p>
+      
+      {users.map((user) => (
+        <div key={user.id}>
+          <div>
+            <p>Nome: {user.name}</p>
+            <p>Idade: {user.age} </p>
+            <p>Email: {user.email} </p>
+          </div>
+          <button>
+            <img src={Trash} />
+          </button>
         </div>
-        <button>
-          <img src={Trash}/>
-        </button>
-      </div>
-      </div>
-  )
+      ))}
+    </div>
+  );
 }
 
-
-export default Home
+export default Home;
